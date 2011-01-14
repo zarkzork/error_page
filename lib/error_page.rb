538a24df899020b1db1df5732077fed5cbec7650
zark zork <zarkzork@gmail.com>
2011-01-14 21:17:56 +0300

    decluttering global namespace

--- lib/error_page.rb
+++ lib/error_page.rb
@@ -1,1 +1,13 @@
-autoload :ErrorPage, 'error_page/middleware'
+
+
+module ErrorPage
+
+  autoload :Middleware, 'error_page/middleware'
+
+  def self.new(*args)
+    Middleware.new
+  end
+  
+  
+end
+
--- lib/error_page/renderer.rb
+++ lib/error_page/renderer.rb
@@ -1,37 +1,41 @@
 require 'tilt'
 
-class Renderer
+module ErrorPage
 
-  Default_options = {
-    :path => 'views',
-    :render_template => true,
-    :layout => :layout
-  }
-  
-  def initialize(options = {})
-    @options = Default_options.merge(options)
-    @cache = Tilt::Cache.new
-  end
+  class Renderer
 
-  Tilt.mappings.each_key do |key|
-    define_method(key.to_sym) do |page|
-      layout_path = File.join(@options[:path],
-                              "#{@options[:layout]}.#{key}")
-      template_path = File.join(@options[:path], "#{page}.#{key}")
-      layout = compile(layout_path)
-      template = compile(template_path)
-      layout.render{ template.render }
+    Default_options = {
+      :path => 'views',
+      :render_template => true,
+      :layout => :layout
+    }
+    
+    def initialize(options = {})
+      @options = Default_options.merge(options)
+      @cache = Tilt::Cache.new
     end
+
+    Tilt.mappings.each_key do |key|
+      define_method(key.to_sym) do |page|
+        layout_path = File.join(@options[:path],
+                                "#{@options[:layout]}.#{key}")
+        template_path = File.join(@options[:path], "#{page}.#{key}")
+        layout = compile(layout_path)
+        template = compile(template_path)
+        layout.render{ template.render }
+      end
+    end
+
+    private
+
+    def compile(path)
+      @cache.fetch(path) { Tilt.new(path)}
+    end
+
+    def set(key, value)
+      @options[key] = value
+    end
+    
   end
 
-  private
-
-  def compile(path)
-    @cache.fetch(path) { Tilt.new(path)}
-  end
-
-  def set(key, value)
-    @options[key] = value
-  end
-  
 end
